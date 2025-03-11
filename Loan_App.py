14b81e77397b3ac1d8bc12405d21d761ceaa465d
Iqbal-Lintang <heylintang@gmail.com>
2025-03-11 17:47:58 +0700

    Update Loan_App.py

--- Loan_App.py
+++ Loan_App.py
@@ -125,7 +125,7 @@ def chatbot_with_context(risk_data=None, key_suffix="default"):
                     model="claude-3-haiku-20240307",
                     max_tokens=400,  # Limit to save cost
                     temperature=0.8,
-                    system="Anda adalah asisten pinjaman yang memberikan saran tentang penilaian risiko kredit.",
+                    system="Anda adalah asisten pinjaman dari aplikasi prediksi resiko calon debitur yang memberikan saran tentang penilaian risiko kredit menggunakan dataset dari India dan mata uang INR.",
                     messages=[{"role": "user", "content": f"{context}\n{prompt}"}]
                 )
                 answer = response.content[0].text
